b3e8253b871ad9c1a9ea46befe500a503cfbb09e
Bruno Empke <brunoempke@Brunos-MacBook-Pro.local>
2025-01-17 15:22:12 -0300

    Finalizando primeira página

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,7 +3,7 @@ import logo from "../../../public/assets/logo.png";
 
 export default function Header() {
     return (
-        <div className="flex justify-center items-center m-auto text-center p-8">
+        <div className="flex justify-center items-center m-auto text-center p-8 border-b-[0.5px] border-neutras-gray">
             <Image src={logo} alt="logo" />
         </div>
     )
--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,13 +1,22 @@
+import Image from "next/image"
+import circuloDourado from "../../../public/assets/circulo-dourado.png";
+import setaDireita from "../../../public/assets/seta-direita.svg";
+
 export default function Main() {
     return (
-        <div className="flex justify-center items-center text-center h-screen text-aviso-alert gap-[10em]">
-            <div>Imagem</div>
-            <div className="flex flex-col justify-center text-center max-w-80 gap-4">
-                <h2 className="text-primarias-black font-montserrat">Parabéns por chegar até aqui!</h2>
+        <div className="flex justify-center items-center text-center h-[750px] text-aviso-alert gap-[10em] max-w-screen-2xl overflow-hidden">
+            <Image src={circuloDourado} alt="circulo-dourado" className="max-w-[600px]" />
+            <div className="flex flex-col justify-center text-left max-w-80 gap-4">
+                <h2 className="text-primarias-black font-montserrat text-[32px] font-extrabold [text-shadow:0_0_4px_#FFFF00]">Parabéns por chegar até aqui!</h2>
                 <p className="text-neutras-prata">Prepare-se para uma jornada incrível! Na D3, você terá a oportunidade de fazer parte de um time talentoso e trabalhar em projetos desafiadores.</p>
                 <p className="text-neutras-white">Tire uma copia do arquivo figma e envie para o email fornecido na vaga.</p>
-                <button className="mt-8 p-3 bg-primarias-black text-neutras-black font-inconsolata">
-                    Iniciar quiz
+                <button className="mt-8 p-2 bg-primarias-black text-neutras-black font-inconsolata max-w-[180px]">
+                    <div className="flex flex-row justify-center text-center items-center gap-2">
+                        Iniciar quiz
+                        <Image
+                            src={setaDireita} alt="seta-direita"
+                        />
+                    </div>
                 </button>
             </div>
         </div>
